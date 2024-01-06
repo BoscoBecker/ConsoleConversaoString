d1279d5560a4345d673a9d3c8b6d9b8271e724ff
DESKTOP-G0TT3JA\Usuário <joao.jbosco@gmail.com>
2024-01-06 14:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleConversaoString/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Conversão implicita
+float x = 4.5f; //4 bytes
+double y = x;   // 8 bytes
+
+Console.WriteLine(y);
+
+// Conversão explicita
+double a; // 8 bytes
+int b; // 4 bytes
+
+a = 5.1;
+b = (int)a;
+
+Console.WriteLine(b);
+
+int aa = 5;
+int bb = 2;
+
+double resultado = aa / bb; // Gera uma divião inteira
+Console.WriteLine(resultado);
+
+double resultado2 = (double)aa / bb; // Gera uma divião double
+Console.WriteLine(resultado2);
